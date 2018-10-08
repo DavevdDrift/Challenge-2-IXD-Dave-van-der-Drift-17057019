19e5c469b87a781d236d6bfe71b565d51f18d10d
DavevdDrift <43164376+DavevdDrift@users.noreply.github.com>
2018-10-08 17:27:08 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,1 @@
-document.getElementsByClassName('menutoggle')[0].onclick = function () {
-	var menu = document.getElementById('menu');
-	if(menu.className == "active") {
-		menu.className = "inactive";
-	}else{
-		menu.className = "active";
-	} 
-}
+document.getElementsByClassName('menutoggle')[0].onclick=function(){var menu=document.getElementById('menu');if(menu.className=="active"){menu.className="inactive";}else{menu.className="active";}}
